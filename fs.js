dfac8d60470ec47495d0c5f0795555e74dbdcfaa
dimon-ton <saichon2namoom@gmail.com>
2022-05-05 17:59:17 +0700

    change comment attribute

--- fs.js
+++ fs.js
@@ -1,18 +1,18 @@
 //Blocking
 
 const fs = require('fs')
+/*
+const data = fs.readFileSync('myFile/input.txt','utf-8')
 
-// const data = fs.readFileSync('myFile/input.txt','utf-8')
+console.log(data)
+console.log('finish work')
 
-// console.log(data)
-// console.log('finish work')
+//เขียนไฟล์
 
-// //เขียนไฟล์
-
-// const outputtxt = `hello node.js\n ${data} \n ไฟล์ถูกเขียนเมื่อ ${new Date}`
-// fs.writeFileSync('myFile/output.txt',outputtxt)
-// console.log('เขียนไฟล์เรียบร้อย')
-
+const outputtxt = `hello node.js\n ${data} \n ไฟล์ถูกเขียนเมื่อ ${new Date}`
+fs.writeFileSync('myFile/output.txt',outputtxt)
+console.log('เขียนไฟล์เรียบร้อย')
+*/
 fs.readFile('myFile/input.txt','utf-8',(err,data)=>{
     if(err) return console.log('เกิดข้อผิดพลาด',err)
     const outputtxt = `Hello node.js\n${data}\nไฟล์นี้ถูกเขียนเมื่อ${new Date()}`
