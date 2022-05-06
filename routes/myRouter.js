481d95eeda25feaf18af9bdf8320e7dc28ea80d6
dimon-ton <saichon2namoom@gmail.com>
2022-05-06 19:51:02 +0700

    condition if/else

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get('/',(req,res)=>{
     const name = "pimon tungratog"
-    const age = 35
+    const age = 17
     const address = "<h3>กรุงเทพ หาดใหญ่</h3>"
     res.render('index',{name:name,age:age,address:address})
 })
