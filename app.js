6bcea5dac08b21c337b8ac6937c874e3fc4f29a3
dimon-ton <saichon2namoom@gmail.com>
2022-05-06 15:48:45 +0700

    views & template

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const path = require('path')
+const router = require('./routes/myRouter')
 const app = express()
 
+app.use(router)
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
 app.use(express.static(path.join(__dirname,'public')))
 
 app.listen(8080,()=>{
