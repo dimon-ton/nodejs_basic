645c3b69d3d4e441c768500ed31c4138f00e3376
dimon-ton <saichon2namoom@gmail.com>
2022-05-06 20:22:56 +0700

    send data as Array

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -2,12 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-    const name = "pimon tungratog"
-    const age = 17
-    const address = "<h3>กรุงเทพ หาดใหญ่</h3>"
-    res.render('index',{name:name,age:age,address:address})
+    const products = ["clothes","fan","earbud","keaboard","table","router"]
+    res.render('index',{products:products})
 })
-const path = require('path')
+
 
 
 
