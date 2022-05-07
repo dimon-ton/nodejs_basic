c484730d0ba96dd6f1714622374d67de2b93ce6c
dimon-ton <saichon2namoom@gmail.com>
2022-05-07 16:15:50 +0700

    display data

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -21,16 +21,10 @@ const upload = multer({
 })
 
 router.get('/',(req,res)=>{
-    const products = [
-        {name:"Notebook",price:25000,image:"images/products/product1.png"},  
-        {name:"clothes",price:2000,image:"images/products/product2.png"}, 
-        {name:"earphone",price:800,image:"images/products/product3.png"},
-        {name:"earphone",price:800,image:"images/products/product3.png"},
-        {name:"earphone",price:800,image:"images/products/product3.png"},
-        {name:"earphone",price:800,image:"images/products/product3.png"}
-       
-    ]
-    res.render('index',{products:products})
+    Product.find().exec((err,doc)=>{
+        res.render('index',{products:doc})
+    })
+    
 })
 
 router.get('/addForm',(req,res)=>{
@@ -38,7 +32,10 @@ router.get('/addForm',(req,res)=>{
 })
 
 router.get('/manage',(req,res)=>{
-    res.render('manage')
+    Product.find().exec((err,doc)=>{
+        res.render('manage',{products:doc})
+    })
+   
 })
 
 
